7f56c1fa159a93a572f071a5a227a137ae3d21c2
rauta0127 <takumaurata0127@gmail.com>
2023-11-23 21:32:21 +0900

    first commit

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,5 +1,7 @@
 import NextImage, { ImageProps } from 'next/image'
 
-const Image = ({ ...rest }: ImageProps) => <NextImage {...rest} />
+const Image = ({ src, ...rest }: ImageProps) => (
+    <NextImage src={`/blogv1${src}`} {...rest} />
+)
 
 export default Image
--- next.config.js
+++ next.config.js
@@ -60,6 +60,7 @@ const securityHeaders = [
 module.exports = () => {
   const plugins = [withContentlayer, withBundleAnalyzer]
   return plugins.reduce((acc, next) => next(acc), {
+    basePath: "/blogv1",
     output: 'export',
     reactStrictMode: true,
     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
