ccd9495c07b1c7f5d18e211d3e3fe3e2814a6bcd
rauta0127 <takumaurata0127@gmail.com>
2023-11-30 14:34:33 +0900

    edit base info

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,9 +1,9 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Rauta Blog',
+  author: 'Rauta',
+  headerTitle: 'RautaBlog',
+  description: 'This blog serves as a memo and diary for an individual engineer.',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
