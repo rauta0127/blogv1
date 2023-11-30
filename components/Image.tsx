7fd66a50b78fadc21c827189e1549435af79ad40
rauta0127 <takumaurata0127@gmail.com>
2023-11-30 13:59:45 +0900

    apply ESLint fix to Image.tsx

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,7 +1,5 @@
 import NextImage, { ImageProps } from 'next/image'
 
-const Image = ({ src, ...rest }: ImageProps) => (
-    <NextImage src={`/blogv1${src}`} {...rest} />
-)
+const Image = ({ src, ...rest }: ImageProps) => <NextImage src={`/blogv1${src}`} {...rest} />
 
 export default Image
